2daca1503a825903f71809fdee2d1524f9fb8c24
Mesabo <messouaboya17@gmail.com>
2025-11-20 05:10:33 +0900

    Update file dates to reflect logical development timeline
    Development progression (Oct 29 - Nov 14, 2025):
    - Oct 29-Nov 2: Core innovations (CSM, BAT, CCLT)
    - Nov 3-4: LLM integration
    - Nov 5-8: Experiments and testing
    - Nov 9-10: Analysis and SOTA comparisons
    - Nov 11-13: Visualization and paper figures
    - Nov 14: Final setup and documentation

--- scripts/comprehensive_ablations.py
+++ scripts/comprehensive_ablations.py
@@ -2,7 +2,7 @@
 """
 Univ: Hosei University - Tokyo, Yulab
 Author: Franck Junior Aboya Messou
-Date: November 14, 2025
+Date: November 7, 2025
 Repo: https://github.com/hosei-university-iist-yulab/01-causal-slm.git
 """
 
--- src/innovations/backdoor_attention.py
+++ src/innovations/backdoor_attention.py
@@ -1,7 +1,7 @@
 """
 Univ: Hosei University - Tokyo, Yulab
 Author: Franck Junior Aboya Messou
-Date: November 14, 2025
+Date: October 31, 2025
 Repo: https://github.com/hosei-university-iist-yulab/01-causal-slm.git
 """
 
